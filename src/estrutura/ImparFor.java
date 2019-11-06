0bb4bdc9fbab819d0c695458d79c78840d77cb68
Alexsandro Amancio <alexsandroamancio@leek5uu.onmicrosoft.com>
2019-11-05 23:17:24 -0300

    projeto criado

---
+++ src/estrutura/ImparFor.java
@@ -0,0 +1,23 @@
+package estrutura;
+
+import java.util.Scanner;
+
+public class ImparFor {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		for(int i = 1 ; i<=x; i++) {
+			
+		if(i % 2 != 0)
+			
+			System.out.println(i);
+			
+			sc.close();
+			
+		}
+	}
+
+}
---
+++ src/estrutura/Medidas.java
@@ -0,0 +1,29 @@
+package estrutura;
+
+import java.util.Scanner;
+
+public class Medidas {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			int primeiro = i;
+
+			int segundo = i * i;
+
+			int terceiro = i * i * i;
+
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+
+		}
+
+		sc.close();
+
+	}
+
+}
